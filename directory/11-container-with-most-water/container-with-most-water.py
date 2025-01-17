0e1084d2440e52b45e9e145cb1934a2a578b1e6f
Bkrishna <balakrishnanair24@gmail.com>
2025-01-17 09:02:03 -0500

    Time: 108 ms (28.99%) | Memory: 28.4 MB (36.52%) - LeetSync

--- directory/11-container-with-most-water/container-with-most-water.py
+++ directory/11-container-with-most-water/container-with-most-water.py
@@ -17,3 +17,8 @@ class Solution:
                 right -=1
 
         return max_water
+
+
+# forget about water and focus omn area. area = shorter height * width between to poles.
+#first take left and right as extremes and calcualte area iterating through next areas by comparing it to max_water
+#after the end of the loop return max_water

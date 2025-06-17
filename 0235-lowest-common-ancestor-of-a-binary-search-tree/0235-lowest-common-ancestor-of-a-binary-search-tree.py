6ba16df693df3faab7ea78ffae20d0de666454e5
Bkrishna <balakrishnanair24@gmail.com>
2025-06-17 09:31:16 -0400

    Time: 59 ms (52.64%), Space: 21.2 MB (5.56%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -15,3 +15,5 @@ class Solution:
                 node = node.right
             else:
                 return node
+
+#The time complexity of this solution is O(log n), where n is the number of nodes in the BST, as we are essentially performing a binary search to find the LCA. The space complexity is O(1), as we are only using a constant amount of extra space.

7667a39c56983c381dfe4f3deab572bd6476597f
Bkrishna <balakrishnanair24@gmail.com>
2025-08-03 19:59:11 -0400

    Time: 3 ms (36.52%), Space: 17.8 MB (74.3%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,36 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if not chars:
+            return 0
+
+        write = 1
+        prev_char = chars[0]
+        count = 1
+
+        for char in chars[1:]: 
+            #This is a slicing operation. It creates a new list or new string that is a shallow copy of the original, starting from a specific index and going to the end.
+            if char == prev_char:
+                count += 1
+            else:
+                if count > 1:
+                    for digit in str(count):
+                        chars[write] = digit
+                        write += 1
+                chars[write] = char
+                write += 1
+                prev_char = char
+                count = 1
+
+        if count > 1:
+            for digit in str(count):
+                chars[write] = digit
+                write += 1
+
+        return write
+
+#Iterate through input array chars.
+# For each group of consecutive repeating characters:
+# Write the character to the array at current position write.
+# If the count is greater than 1, write the count to the array at the next position, incrementing write by the length of the count.
+# Increment write by 1.
+# Return new length of array (write).
